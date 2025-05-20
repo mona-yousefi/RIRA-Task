217309d228fa312d1cb89ce46b85899c6b75314f
Mona <m.yousefinejad78@gmail.com>
2025-05-20 19:33:26 +0330

    project has published

---
+++ RIRA Task/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <div>
+      <input type="text" name='moneyUnit' id='moneyUnit' placeholder='مقدار مورد نظر را وارد کنید'/>
+    </div>
+    <div>
+      <p>تبدیل از</p>
+      <select name="" id="">
+        <option value=""></option>
+      </select>
+    </div>
+    <div>
+      <p>به</p>
+      <select name="" id="">
+        <option value=""></option>
+      </select>
+    </div>
+    </>
+  )
+}
+
+export default App
